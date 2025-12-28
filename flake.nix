95131d40a9221c1136b41d48428f774c9e9fb52b
Pascal Wittmann <pascal.wittmann@red6-es.de>
2025-12-28 18:51:53 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "yesod-csp";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.yesod-csp = pkgs.callPackage ./default.nix { nixpkgs = pkgs; };
+      }
+    );
+}
