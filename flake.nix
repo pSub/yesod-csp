0b11fe3f9875e959aecdd47a80dff447d770a92f
Pascal Wittmann <pascal.wittmann@red6-es.de>
2026-06-17 14:49:12 +0200

    Update flake to NixOS-26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "yesod-csp";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
